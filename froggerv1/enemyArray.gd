cb197d1f69263a1df95333e3b90ec2b84331ea86
Isaac <46819262+manpaka@users.noreply.github.com>
2021-02-02 17:59:48 -0500

    Delete froggerv1 directory

--- froggerv1/enemyArray.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-onready var sprite = preload("res://enemy.tscn")
-
-#just a simple for loop creating multiple instences of enemy
-func _ready():
-	for i in range(6):
-		var s = sprite.instance()
-		add_child(s)
